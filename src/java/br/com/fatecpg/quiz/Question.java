f30337dbbc960cee4a014a519243bb7dccd399b4
Weslley Antonio <weslley.miranda@fatec.sp.gov.br>
2018-11-09 14:43:19 -0200

    Create arraylist

--- src/java/br/com/fatecpg/quiz/Question.java
+++ src/java/br/com/fatecpg/quiz/Question.java
@@ -9,6 +9,40 @@ package br.com.fatecpg.quiz;
  *
  * @author usuario
  */
-public class Questoes {
+public class Question {
     
+    private String question;
+    private String answer;
+    private String alternatives[];
+    
+    public Question(String question, String answer, String[] alternatives) {
+        this.question = question;
+        this.answer = answer;
+        this.alternatives = alternatives;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String[] getAlternatives() {
+        return alternatives;
+    }
+
+    public void setAlternatives(String[] alternatives) {
+        this.alternatives = alternatives;
+    }
+       
 }
