941ba4c736f58da5fa810d9a2f17edba9ea6d386
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-11-09 23:41:45 -0200

    Classe para histórico de notas

---
+++ src/java/br/com/fatecpg/quiz/Historic.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fatecpg.quiz;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+
+/**
+ *
+ * @author mateus
+ */
+public class Historic {
+    
+    private String user;
+    private double result;
+    private Date date;
+
+    public Historic(String user, double result, Date date) {
+        this.user = user;
+        this.result = result;
+        this.date = date;
+    }
+    
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void setResult(double result) {
+        this.result = result;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    public static ArrayList<Historic> sortByResult(ArrayList<Historic> historics) {
+        Collections.sort(historics, (Historic h1, Historic h2) -> {
+            if (h1.getResult() > h2.getResult()) {
+                return -1;
+            } else if(h1.getResult() == h2.getResult()){
+                return 0;
+            } else {
+                return 1;
+            }
+        });
+        return historics;
+    }
+    public static ArrayList<Historic> sortByDate(ArrayList<Historic> historics) {
+        Collections.sort(historics, (Historic h1, Historic h2) -> {
+            if (h1.getDate().after(h2.getDate())) {
+                return -1;
+            } else {
+                return 1;
+            }
+        });    
+        return historics;
+    }
+    
+}
